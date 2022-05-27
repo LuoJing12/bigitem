713f09b04aee3f826a84dea79da2f1bfcbcece43
LuoJing <865883532@qq.com>
2022-05-27 15:20:50 +0800

    index用户主页面完成

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,6 +1,24 @@
 //接受的是一个对象,请求的所有数据
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     //发起请求之前,封装路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options);
+    //
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+
+    }
+    options.complete = function (res) {
+        console.log(res);
+        if (
+            res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'
+        ) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
+
+
 })
